a5ed42c6677667cc1c8881e6ad5a45e9b7b3838e
Mario Reder <mario.reder@pm.me>
2025-11-27 06:21:34 +0100

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -19,7 +19,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_HAS_PRIMARY_LOGO": "true",
   "VITE_HAS_SECONDARY_LOGO": "false",
   "VITE_ENABLED_MENUS": "Trading,Leaderboard,Portfolio,Markets",
-  "VITE_CUSTOM_MENUS": "rwd, https://naver.com",
+  "VITE_CUSTOM_MENUS": "Weather Index, https://www.kweather.co.kr/",
   "VITE_ENABLE_SERVICE_DISCLAIMER_DIALOG": "false",
   "VITE_ENABLE_CAMPAIGNS": "false",
   "VITE_TELEGRAM_URL": "",
